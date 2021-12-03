5f131a59d3a14faca978f36c821dcc77cc0f6a80
xuhuiming <xuhuiming6991@gmail.com>
2021-12-03 11:35:12 +0800

    整理代码

--- DesignPattern.ProxyPattern/GiftGiver.cs
+++ DesignPattern.ProxyPattern/GiftGiver.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPattern.ProxyPattern
+﻿namespace DesignPattern.ProxyPattern
 {
     /// <summary>
     /// 送礼物的人 （抽象主题角色）
     /// </summary>
-    public abstract class GiveGiftPerson
+    public abstract class GiftGiver
     {
         //送礼物功能
         public abstract void GiveGift();
@@ -18,7 +12,7 @@ namespace DesignPattern.ProxyPattern
     /// <summary>
     /// 害羞男孩  （真实主题角色）
     /// </summary>
-    public class ShyBoy : GiveGiftPerson
+    public class ShyBoy : GiftGiver
     {
         public override void GiveGift()
         {
@@ -29,13 +23,13 @@ namespace DesignPattern.ProxyPattern
     /// <summary>
     /// 害羞男孩请朋友帮忙送礼物（代理主题角色）
     /// </summary>
-    public class Friend : GiveGiftPerson
+    public class Friend : GiftGiver
     {
         //引用真实主题实例
         ShyBoy shyboy = new ShyBoy();
         public override void GiveGift()
         {
-            //朋友一顿分析后，觉得如花是个好人
+            //朋友增加一顿分析后，觉得如花是个好人，不拦截
             bool IsRuhuaGood = true;
             if (IsRuhuaGood)
             {
--- DesignPattern.ProxyPattern/Program.cs
+++ DesignPattern.ProxyPattern/Program.cs
@@ -1,19 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-// https://www.cnblogs.com/wyy1234/p/10013436.html
-namespace DesignPattern.ProxyPattern
+﻿namespace DesignPattern.ProxyPattern
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //因为不能直接访问到真实对象所以我们不能直接new ShyBoy()去送礼物。
+            //因为不能直接访问到真实对象，所以我们不能直接new ShyBoy()去送礼物。
 
             //代码中使用的是代理对象
             Friend friend = new Friend();
             friend.GiveGift();
-            Console.ReadKey();
         }
     }
 }
+
+// https://www.cnblogs.com/wyy1234/p/10013436.html
